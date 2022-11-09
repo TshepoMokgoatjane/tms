420869464e47c31a7521886fe2f522c61051ae51
Mokgoatjane, Tshepo <tshepo.mokgoatjane@fnb.co.za>
2022-11-09 19:59:26 +0200

    Spring Boot initial commit for TMS - Tenant Management System.

---
+++ src/test/java/za/co/tms/tms/TmsApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.tms.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
