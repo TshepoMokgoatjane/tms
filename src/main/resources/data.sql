9d5453e462f9607be0a49ea66925262e6943059c
Mokgoatjane, Tshepo <tshepo.mokgoatjane@fnb.co.za>
2023-09-11 15:06:14 +0200

    Add Spring JPA configuration and data SQL script.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into tenant(id, created_at, updated_at, alternative_cell_phone_number, cell_phone_number, deposit_paid, email, lease_end_date, lease_start_date, name, number_of_tenants_in_unit, payment_date, prepaid_electricity_meter_number, rental, room_number, surname, tenant_behaviour, title)
+values (400021, CURRENT_DATE(), CURRENT_DATE(), 0715256922, 0865231004, true, 'tim@ibm.com', CURRENT_DATE(), CURRENT_DATE(), 'Timothy', 5, CURRENT_DATE(), '58796320591' , 5000, 'A1', 'Gibson',0, 'Mr.');
+
+insert into tenant(id, created_at, updated_at, alternative_cell_phone_number, cell_phone_number, deposit_paid, email, lease_end_date, lease_start_date, name, number_of_tenants_in_unit, payment_date, prepaid_electricity_meter_number, rental, room_number, surname, tenant_behaviour, title)
+values (400022, CURRENT_DATE(), CURRENT_DATE(), 0715251210, 0865232240, true, 'billy.jones@yahoo.com', CURRENT_DATE(), CURRENT_DATE(), 'Billy', 1, CURRENT_DATE(), '58796317991' , 45000, 'A10', 'Jones',0, 'Mr.');
+
+insert into tenant(id, created_at, updated_at, alternative_cell_phone_number, cell_phone_number, deposit_paid, email, lease_end_date, lease_start_date, name, number_of_tenants_in_unit, payment_date, prepaid_electricity_meter_number, rental, room_number, surname, tenant_behaviour, title)
+values (400023, CURRENT_DATE(), CURRENT_DATE(), 0716006911, 0865551004, false, 'paul.stones@google.com', CURRENT_DATE(), CURRENT_DATE(), 'Paul', 3, CURRENT_DATE(), '58905810544' , 3500, 'A4', 'Stones',0, 'Mr.');
+
+insert into tenant(id, created_at, updated_at, alternative_cell_phone_number, cell_phone_number, deposit_paid, email, lease_end_date, lease_start_date, name, number_of_tenants_in_unit, payment_date, prepaid_electricity_meter_number, rental, room_number, surname, tenant_behaviour, title)
+values (400024, CURRENT_DATE(), CURRENT_DATE(), 0745301144, 0865255124, true, 'michelle.davidson@react.com', CURRENT_DATE(), CURRENT_DATE(), 'Michelle', 2, CURRENT_DATE(), '58700323079' , 3500, 'A7', 'Davids',0, 'Miss.');
+
+insert into tenant(id, created_at, updated_at, alternative_cell_phone_number, cell_phone_number, deposit_paid, email, lease_end_date, lease_start_date, name, number_of_tenants_in_unit, payment_date, prepaid_electricity_meter_number, rental, room_number, surname, tenant_behaviour, title)
+values (400025, CURRENT_DATE(), CURRENT_DATE(), 0715578103, 0830120367, false, 'quintinh@takealot.com', CURRENT_DATE(), CURRENT_DATE(), 'Quitin', 1, CURRENT_DATE(), '59333351027' , 3000, 'C1', 'Harris',1, 'Mr.');
+
+insert into tenant(id, created_at, updated_at, alternative_cell_phone_number, cell_phone_number, deposit_paid, email, lease_end_date, lease_start_date, name, number_of_tenants_in_unit, payment_date, prepaid_electricity_meter_number, rental, room_number, surname, tenant_behaviour, title)
+values (400026, CURRENT_DATE(), CURRENT_DATE(), 0720256378, 0815231999, true, 'nelly.fertado@sonymusic.com', CURRENT_DATE(), CURRENT_DATE(), 'Nelly', 4, CURRENT_DATE(), '57963296377' , 5000, 'B1', 'Fertado',0, 'Mrs.');
