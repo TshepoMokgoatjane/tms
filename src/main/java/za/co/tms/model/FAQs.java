57f61bacfd971dc35f5343a7110cd5bb8f8bae03
Mokgoatjane, Tshepo <tshepo.mokgoatjane@fnb.co.za>
2024-06-13 19:50:58 +0200

    Created a FAQs Entity Model #59

---
+++ src/main/java/za/co/tms/model/FAQs.java
@@ -0,0 +1,31 @@
+package za.co.tms.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class FAQs implements Serializable {/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4830337221487671406L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Integer id;
+	
+	private String question;
+	private String answer;
+	
+}
