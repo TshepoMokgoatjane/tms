ef55336f5c38ac7176fc24cde65f8d6eb4d67b71
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2025-10-11 23:28:00 +0200

    Added more assertions to tests - JwtIntegrationTest.java #72

--- src/test/java/za/co/tms/JwtIntegrationTest.java
+++ src/test/java/za/co/tms/JwtIntegrationTest.java
@@ -1,5 +1,6 @@
 package za.co.tms;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import za.co.tms.repository.UserInfoRepository;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -69,9 +72,13 @@ public class JwtIntegrationTest {
 
         String token = result.getResponse().getContentAsString();
 
+        Assertions.assertNotNull(token);
+        Assertions.assertTrue(token.startsWith("ey")); // JWT tokens typically starts with 'ey'
+
         // Step 3: Access protected endpoint
         mockMvc.perform(get("/auth/user/userProfile")
                 .header("Authorization", "Bearer " + token))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Welcome to User Profile")));
     }
 }
