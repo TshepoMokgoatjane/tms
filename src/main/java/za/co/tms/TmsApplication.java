f1ea7789c033f51f458554beeee37984a4504505
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2026-07-17 10:30:44 +0200

    Make Contact Us email sending asynchronous #123
    - Add @EnableAsync to TmsApplication
    - Add @Async to EmailService.sendContactUsNotification()
    - Form now responds instantly, email sends in background thread
    - Eliminates 2-3 minute wait time on Contact Us submission

--- src/main/java/za/co/tms/TmsApplication.java
+++ src/main/java/za/co/tms/TmsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAsync
 public class TmsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/za/co/tms/service/EmailService.java
+++ src/main/java/za/co/tms/service/EmailService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import za.co.tms.domain.ContactUs;
 import za.co.tms.domain.Tenant;
@@ -22,6 +23,7 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    @Async
     public void sendContactUsNotification(ContactUs contactUs) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
