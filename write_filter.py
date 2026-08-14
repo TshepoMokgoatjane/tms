de0f09557d26b22f6f58f925ded97d33c4575cec
F5103762 <Tshepo.Mokgoatjane@fnb.o.za>
2026-08-14 17:42:16 +0200

    chore: remove unused dev utility scripts (part1.txt, write_filter.py)

--- write_filter.py
+++
@@ -1,4 +0,0 @@
-import os
-filepath = r'c:\devSpace\tms\src\main\java\za\co\tms\jwt\JwtAuthenticationFilter.java'
-content = open(filepath, 'r', encoding='utf-8').read()
-print('Current file length:', len(content))
